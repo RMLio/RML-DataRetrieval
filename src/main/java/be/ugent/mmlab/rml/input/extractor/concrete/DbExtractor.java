b0289e2bc3e3d81060d782164c43635f9c70a5ba
andimou <anastasia.dimou@ugent.be>
2015-08-08 20:46:00 +0200

    added spring and mysql dependencies

--- src/main/java/be/ugent/mmlab/rml/input/extractor/concrete/DbExtractor.java
+++ src/main/java/be/ugent/mmlab/rml/input/extractor/concrete/DbExtractor.java
@@ -5,6 +5,11 @@ import be.ugent.mmlab.rml.input.extractor.AbstractInputExtractor;
 import java.util.Set;
 import org.openrdf.model.Resource;
 import org.openrdf.repository.Repository;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.vendor.Database;
 
 /**
  * RML - Data Retrieval Handler : DbExtractor
@@ -12,15 +17,28 @@ import org.openrdf.repository.Repository;
  * @author andimou
  */
 public class DbExtractor extends AbstractInputExtractor {
+    
+    private JdbcOperations jdbcOperations;
 
         @Override
         public Set<String> extractStringTemplate(String source) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+            
+            this.jdbcOperations = new JdbcTemplate();
+            
+            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("app-context.xml");
+            Database db = (Database) context.getBean("db");
+            return null;
     }
-
-        @Override
-        public Set<InputSource> extractInput(Repository repository, Resource resource) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        
+    @Override
+    public Set<InputSource> extractInput(Repository repository, Resource resource) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost/test_db");
+        dataSource.setUsername("nata");
+        dataSource.setPassword("nata");
+        
+        return null;
     }
     
 }
